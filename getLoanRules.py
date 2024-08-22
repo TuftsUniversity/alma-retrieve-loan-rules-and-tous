f2086eac488653871c509b34e8e5de26b48dd92f
Mark Eaton <MarkEEaton@users.noreply.github.com>
2024-08-22 10:03:55 -0400

    run black code formatter on python scripts

--- getLoanRules.py
+++ getLoanRules.py
@@ -35,22 +35,28 @@ import numpy as np
 
 import secrets_local
 
-sys.path.append('scripts/')
+sys.path.append("scripts/")
 from functions import *
 
 
-if " " in secrets_local.alma_base_url or " " in secrets_local.username or " " in secrets_local.password:
-    print("Please set your admin account credentials and Alma URL in the secrets_local.py file")
+if (
+    " " in secrets_local.alma_base_url
+    or " " in secrets_local.username
+    or " " in secrets_local.password
+):
+    print(
+        "Please set your admin account credentials and Alma URL in the secrets_local.py file"
+    )
     sys.exit(1)
 
 oDir = "./Output"
 if not os.path.isdir(oDir) or not os.path.exists(oDir):
-	os.makedirs(oDir)
-#for windows
-home_directory_chromedriver_path = os.path.expanduser('~') + "/chromedriver.exe"
+    os.makedirs(oDir)
+# for windows
+home_directory_chromedriver_path = os.path.expanduser("~") + "/chromedriver.exe"
 driver = webdriver.Chrome(ChromeDriverManager().install())
-#driver = webdriver.Chrome()#home_directory_chromedriver_path)
-#sys.exit()
+# driver = webdriver.Chrome()#home_directory_chromedriver_path)
+# sys.exit()
 
 driver.get(secrets_local.alma_base_url)
 time.sleep(5)
@@ -59,7 +65,7 @@ time.sleep(8)
 
 full_units = navigate_to_fulfillment_units(driver, secrets_local.alma_base_url)
 
-#get number of fulfillment full_units
+# get number of fulfillment full_units
 time.sleep(2)
 full_unit_count = get_fulfillment_unit_count(driver)
 
@@ -69,92 +75,91 @@ fulfillment_unit_df = pd.read_html(html)[0]
 
 print(fulfillment_unit_df)
 
-#iterate through fulfillment full_units
+# iterate through fulfillment full_units
 x = 2
 
 print("Fulfillment unit count: " + str(full_unit_count))
-while x < 3:#full_unit_count:
+while x < 3:  # full_unit_count:
 
-
-
-    fulfillment_unit = fulfillment_unit_df.loc[x, 'sortable']
+    fulfillment_unit = fulfillment_unit_df.loc[x, "sortable"]
 
     return_list = navigate_to_rules_tab_get_lists(driver, x)
-
 
     rules_df = return_list[0]
     locations_list = return_list[1]
 
     rule_count = len(rules_df)
 
-
     y = 0
     start = y
     print("Number of rules in Fulfillment unit " + str(rule_count))
 
-
-
     while y < rule_count:
 
-        rule_df = pd.DataFrame(columns=['Fullfilment Unit', 'Possible Locations', 'Enabled', 'Rule Name', 'Unnamed: 0', 'Unnamed: 4', 'Output'])
+        rule_df = pd.DataFrame(
+            columns=[
+                "Fullfilment Unit",
+                "Possible Locations",
+                "Enabled",
+                "Rule Name",
+                "Unnamed: 0",
+                "Unnamed: 4",
+                "Output",
+            ]
+        )
 
-        s = pd.Series([None,None,None,None,None,None,None],index=['Fullfilment Unit', 'Possible Locations', 'Enabled', 'Rule Name', 'Unnamed: 0', 'Unnamed: 4', 'Output'])
+        s = pd.Series(
+            [None, None, None, None, None, None, None],
+            index=[
+                "Fullfilment Unit",
+                "Possible Locations",
+                "Enabled",
+                "Rule Name",
+                "Unnamed: 0",
+                "Unnamed: 4",
+                "Output",
+            ],
+        )
         rule_df = pd.concat([rule_df, pd.DataFrame([s])], ignore_index=True)
 
-        rule_name = rules_df.loc[y, 'Rule Name']
+        rule_name = rules_df.loc[y, "Rule Name"]
         print("Rule name: " + str(rule_name))
-        output = rules_df.loc[y, 'Output']
+        output = rules_df.loc[y, "Output"]
 
-
-        fulfillment_unit = fulfillment_unit.replace("\/", '-')
-        fulfillment_unit = fulfillment_unit.replace("\\", '-')
-        rule_df['Fullfilment Unit'] = fulfillment_unit
-        rule_df['Possible Locations'] = ",".join(locations_list)
-        rule_df['Rule Name'] = rule_name
-        rule_df['Output'] = output
-
-
-
-
+        fulfillment_unit = fulfillment_unit.replace("\/", "-")
+        fulfillment_unit = fulfillment_unit.replace("\\", "-")
+        rule_df["Fullfilment Unit"] = fulfillment_unit
+        rule_df["Possible Locations"] = ",".join(locations_list)
+        rule_df["Rule Name"] = rule_name
+        rule_df["Output"] = output
 
         time.sleep(2)
 
         enabled_value = ""
 
-
-
-
         enabled_value = get_enabled_value(driver, y)
 
-        rule_df['Enabled'] = enabled_value
+        rule_df["Enabled"] = enabled_value
 
         navigate_to_loan_rule(driver, y)
 
-
-
-
         time.sleep(2)
-
-
 
         parameter_string = get_parameter_list(driver)
 
-        rule_df['Parameter'] = parameter_string
-
+        rule_df["Parameter"] = parameter_string
 
         navigate_to_tou(driver)
 
         time.sleep(2)
         tou_series = get_tou_as_series(driver)
 
-
         rule_df = rule_df.join(tou_series.to_frame(rule_df.index[0]).T)
 
-        rule_df = rule_df.drop_duplicates(subset=['Rule Name'])
+        rule_df = rule_df.drop_duplicates(subset=["Rule Name"])
 
         if y == start:
             ruless_df = rule_df.copy()
-
 
         ###################################
         ####    Go back to start to get the
@@ -162,27 +167,31 @@ while x < 3:#full_unit_count:
 
         time.sleep(1)
         wait = WebDriverWait(driver, 20)
-        element = wait.until(ec.visibility_of_element_located((By.XPATH, '//*[@id="generic_back_button"]')))
+        element = wait.until(
+            ec.visibility_of_element_located(
+                (By.XPATH, '//*[@id="generic_back_button"]')
+            )
+        )
         element.click()
         time.sleep(2.5)
         wait = WebDriverWait(driver, 20)
         try:
-            element = wait.until(ec.visibility_of_element_located((By.XPATH, '//*[@id="generic_back_button"]')))
+            element = wait.until(
+                ec.visibility_of_element_located(
+                    (By.XPATH, '//*[@id="generic_back_button"]')
+                )
+            )
             element.click()
         except:
             print("No back button needed")
         ruless_df = pd.concat([ruless_df, rule_df], ignore_index=True)
 
-
         y += 1
-    ruless_df = ruless_df.drop_duplicates(subset=['Rule Name'])
+    ruless_df = ruless_df.drop_duplicates(subset=["Rule Name"])
 
-    fulfillment_unit = re.sub(r'[^A-Za-z0-9 ]','-', fulfillment_unit)
+    fulfillment_unit = re.sub(r"[^A-Za-z0-9 ]", "-", fulfillment_unit)
 
     ruless_df.to_excel("Output/" + fulfillment_unit + ".xlsx", index=False)
     navigate_to_fulfillment_units(driver, secrets_local.alma_base_url)
-
-
-
 
     x += 1
--- scripts/merge_functions.py
+++ scripts/merge_functions.py
@@ -34,69 +34,121 @@ if not os.path.isdir(oDir) or not os.path.exists(oDir):
 
 def mergeAndPresent(rule_df):
 
-
     x = 0
     rule_df = rule_df.reset_index()
     rule_df = rule_df.fillna("None")
-    rule_df = rule_df.drop(['Overdue Notification Fine Type 1', 'Overdue Notification Fine Type 2', 'Overdue Notification Fine Type 3', 'Overdue Notification Fine Type 4', 'Overdue Notification Fine Type 5'], axis=1)
-    #print(rule_df)
-    rule_df.loc[rule_df['Operator'] == 0, 'Operator'] = "Equals"
+    rule_df = rule_df.drop(
+        [
+            "Overdue Notification Fine Type 1",
+            "Overdue Notification Fine Type 2",
+            "Overdue Notification Fine Type 3",
+            "Overdue Notification Fine Type 4",
+            "Overdue Notification Fine Type 5",
+        ],
+        axis=1,
+    )
+    # print(rule_df)
+    rule_df.loc[rule_df["Operator"] == 0, "Operator"] = "Equals"
     while x < len(rule_df):
 
         # print(rule_df.shape)
         # print(rule_df)
         # sys.exit()
         # if rule_df.iloc[x]['Unnamed: 0'] == 1:
-        columns_to_apply_list = ["Is Loanable", "Is Recallable", "Due Date", "Requested Item Due Date", "Recall Period", "Renew Fee", "Lost Item Fine", "Lost Item Replacement Fee", "Lost Item Replacement Fee Refund Ratio", "Maximum Fine", "Overdue Fine", "Recalled Overdue Fine", "Grace Period", "Is Renewable", "Maximum Renewal Period", "Closed Library Due Date Management", "Cancelled Recall Due Date", "Block When Overdue", "Maximum Period For Overdue Block", "Reloan Limit", "Time frame when loan renewal is allowed"]
+        columns_to_apply_list = [
+            "Is Loanable",
+            "Is Recallable",
+            "Due Date",
+            "Requested Item Due Date",
+            "Recall Period",
+            "Renew Fee",
+            "Lost Item Fine",
+            "Lost Item Replacement Fee",
+            "Lost Item Replacement Fee Refund Ratio",
+            "Maximum Fine",
+            "Overdue Fine",
+            "Recalled Overdue Fine",
+            "Grace Period",
+            "Is Renewable",
+            "Maximum Renewal Period",
+            "Closed Library Due Date Management",
+            "Cancelled Recall Due Date",
+            "Block When Overdue",
+            "Maximum Period For Overdue Block",
+            "Reloan Limit",
+            "Time frame when loan renewal is allowed",
+        ]
 
         # part of this row of the dataframe with TOU columns and values
         columns_to_apply = rule_df.loc[x, rule_df.columns.isin(columns_to_apply_list)]
 
         # part of this row of the dataframe that has the loan rule information
         # not used
-        #match_columns = rule_df.loc[x, ~rule_df.columns.isin(columns_to_apply_list)]
+        # match_columns = rule_df.loc[x, ~rule_df.columns.isin(columns_to_apply_list)]
 
-
-
-
-
-        rule_name = rule_df.loc[x, 'Rule Name']
+        rule_name = rule_df.loc[x, "Rule Name"]
         # print("Rule df in merge script")
         # print(rule_df)
         # sys.exit()
 
-        #broadcast the TOUs in each row where the rule name matches this
-        change_rule_df = rule_df.loc[rule_df['Rule Name'] == rule_name]
+        # broadcast the TOUs in each row where the rule name matches this
+        change_rule_df = rule_df.loc[rule_df["Rule Name"] == rule_name]
         change_rule_df[columns_to_apply_list] = columns_to_apply.values
 
-        #now integrate these changes back into the master dataframe
+        # now integrate these changes back into the master dataframe
         rule_df.update(change_rule_df)
 
         if rule_name == "End of Day All Patrons Ginn Laptops":
 
-
-            rule_df = rule_df.drop_duplicates(subset=['Rule Name', 'Value'])
+            rule_df = rule_df.drop_duplicates(subset=["Rule Name", "Value"])
 
         x += 1
 
-
-
-
-
-
-    rule_df = rule_df.drop(['Unnamed: 0', 'Unnamed: 4'], axis=1)
-    rule_df['Parameter'] = rule_df['Name'].astype('string') + " " + rule_df["Operator"].astype('string') + " " + rule_df['Value'].astype('string')
+    rule_df = rule_df.drop(["Unnamed: 0", "Unnamed: 4"], axis=1)
+    rule_df["Parameter"] = (
+        rule_df["Name"].astype("string")
+        + " "
+        + rule_df["Operator"].astype("string")
+        + " "
+        + rule_df["Value"].astype("string")
+    )
 
     print("Rule df in merge with parameter column")
     print(rule_df)
 
+    # wrap up multiple lines for each loan rule, and join the separte parameters into a list
+    columns_to_apply_list_master = [
+        "Fullfilment Unit",
+        "Possible Locations",
+        "Rule Name",
+        "Output",
+        "Is Loanable",
+        "Is Recallable",
+        "Due Date",
+        "Requested Item Due Date",
+        "Recall Period",
+        "Renew Fee",
+        "Lost Item Fine",
+        "Lost Item Replacement Fee",
+        "Lost Item Replacement Fee Refund Ratio",
+        "Maximum Fine",
+        "Overdue Fine",
+        "Recalled Overdue Fine",
+        "Grace Period",
+        "Is Renewable",
+        "Maximum Renewal Period",
+        "Closed Library Due Date Management",
+        "Cancelled Recall Due Date",
+        "Block When Overdue",
+        "Maximum Period For Overdue Block",
+        "Reloan Limit",
+        "Time frame when loan renewal is allowed",
+    ]
+    rule_df["Parameter"] = rule_df.groupby(columns_to_apply_list_master, dropna=False)[
+        "Parameter"
+    ].transform(lambda x: ";".join(x))
 
-
-    #wrap up multiple lines for each loan rule, and join the separte parameters into a list
-    columns_to_apply_list_master = ['Fullfilment Unit', 'Possible Locations', 'Rule Name', 'Output', "Is Loanable", "Is Recallable", "Due Date", "Requested Item Due Date", "Recall Period", "Renew Fee", "Lost Item Fine", "Lost Item Replacement Fee", "Lost Item Replacement Fee Refund Ratio", "Maximum Fine", "Overdue Fine", "Recalled Overdue Fine", "Grace Period", "Is Renewable", "Maximum Renewal Period", "Closed Library Due Date Management", "Cancelled Recall Due Date", "Block When Overdue", "Maximum Period For Overdue Block", "Reloan Limit", "Time frame when loan renewal is allowed"]
-    rule_df['Parameter'] = rule_df.groupby(columns_to_apply_list_master, dropna=False)['Parameter'].transform(lambda x: ';'.join(x))
-
-    rule_df = rule_df.drop_duplicates(subset=['Rule Name'])
+    rule_df = rule_df.drop_duplicates(subset=["Rule Name"])
 
     print(rule_df)
     return rule_df
